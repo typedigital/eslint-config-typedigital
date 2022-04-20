fd68e6a7f4dd4f49b1bf548ad7eb0299bb84c34d
bob606 <saliu.bah654@gmail.com>
2022-04-20 07:39:23 +0200

    chore: implemented review

--- base.js
+++ base.js
@@ -489,7 +489,7 @@ module.exports = {
     'promise/param-names': 'error', // https://github.com/xjamundx/eslint-plugin-promise/blob/master/docs/rules/param-names.md
     // * it's better to use one syntax so the await syntax should be our standard and in some cases the `then` syntax can be used
     'promise/prefer-await-to-callbacks': 'warn', // https://github.com/xjamundx/eslint-plugin-promise/blob/master/docs/rules/prefer-await-to-callbacks.md
-    // * it's better to use one syntax so the await syntax should be our standard
+    'promise/prefer-await-to-then': 'off', // https://github.com/xjamundx/eslint-plugin-promise/blob/master/docs/rules/prefer-await-to-then.md
     'promise/valid-params': 'error', // https://github.com/xjamundx/eslint-plugin-promise/blob/master/docs/rules/valid-params.md
     'quote-props': ['error', 'consistent-as-needed'], // https://eslint.org/docs/rules/quote-props
     'quotes': ['error', 'single', { allowTemplateLiterals: true }], // handled by @babel/quotes
