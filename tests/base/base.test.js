1583daaf047879ce8bc2a2027cf0b403754477e6
lucasfalkowsky <lucas.falkowsky@typedigital.de>
2023-02-22 14:58:55 +0100

    feat: test for params for function

---
+++ tests/base/base.test.js
@@ -0,0 +1,15 @@
+import { describe, it, expect } from 'vitest';
+import { ESLint } from 'eslint';
+
+describe('base config', () => {
+  it('should allow more then 3 params passed to function', async () => {
+    // arrange
+    const linter = new ESLint();
+    const fileName = 'tests/base/function-param-test.js';
+
+    // act
+    const [result] = await linter.lintFiles([fileName]);
+
+    expect(result.errorCount).toBe(0);
+  });
+});
