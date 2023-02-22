b85d22887ecadad5f05e3c44661268041c67537b
lucasfalkowsky <lucas.falkowsky@typedigital.de>
2023-02-22 14:49:44 +0100

    fix: test array spreading with eslintrc for base

---
+++ tests/base/base.test.js
@@ -0,0 +1,15 @@
+import { describe, it, expect } from 'vitest';
+import { ESLint } from 'eslint';
+
+describe('base config', () => {
+  it('should allow spreading of array elements', async () => {
+    // arrange
+    const linter = new ESLint();
+    const fileName = 'tests/base/array-spread.js';
+
+    // act
+    const [result] = await linter.lintFiles([fileName]);
+
+    expect(result.errorCount).toBe(0);
+  });
+});
